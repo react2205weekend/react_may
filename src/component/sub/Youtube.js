bcc45b7f84390ca09b3833c0a876222911159f85
tubi55 <tubi55@nate.com>
2022-05-29 11:50:03 +0900

    popup호출

--- src/component/sub/Youtube.js
+++ src/component/sub/Youtube.js
@@ -1,9 +1,11 @@
 import Layout from '../common/Layout';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Popup from '../common/Popup';
 
 function Youtube() {
 	const [vids, setVids] = useState([]);
+	const [open, setOpen] = useState(false);
 	const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
 	const playlist = 'PLHtvRFLN5v-W-izd7V4JH2L4-RTW0WRi3';
 	const num = 8;
@@ -17,26 +19,30 @@ function Youtube() {
 	}, []);
 
 	return (
-		<Layout name={'Youtube'}>
-			{vids.map((vid, idx) => {
-				const tit = vid.snippet.title;
-				const desc = vid.snippet.description;
-				const date = vid.snippet.publishedAt;
+		<>
+			<Layout name={'Youtube'}>
+				{vids.map((vid, idx) => {
+					const tit = vid.snippet.title;
+					const desc = vid.snippet.description;
+					const date = vid.snippet.publishedAt;
 
-				return (
-					<article key={idx}>
-						<h2>{tit.length > 20 ? tit.substr(0, 20) + '...' : tit}</h2>
-						<div className='txt'>
-							<p>{desc.length > 200 ? desc.substr(0, 200) + '...' : desc}</p>
-							<span>{date.split('T')[0]}</span>
-						</div>
-						<div className='pic'>
-							<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
-						</div>
-					</article>
-				);
-			})}
-		</Layout>
+					return (
+						<article key={idx}>
+							<h2>{tit.length > 20 ? tit.substr(0, 20) + '...' : tit}</h2>
+							<div className='txt'>
+								<p>{desc.length > 200 ? desc.substr(0, 200) + '...' : desc}</p>
+								<span>{date.split('T')[0]}</span>
+							</div>
+							<div className='pic' onClick={() => setOpen(true)}>
+								<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+							</div>
+						</article>
+					);
+				})}
+			</Layout>
+
+			{open ? <Popup setOpen={setOpen} /> : null}
+		</>
 	);
 }
 
