be3aefa3fa43c5a514eee272346953b6868b2b88
tubi55 <tubi55@nate.com>
2022-06-04 10:24:49 +0900

    기본 지도 출력

--- src/component/sub/Location.js
+++ src/component/sub/Location.js
@@ -1,9 +1,21 @@
 import Layout from '../common/Layout';
+import { useEffect, useRef } from 'react';
 
 function Location() {
+	const { kakao } = window;
+	const container = useRef(null);
+	const options = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+
+	useEffect(() => {
+		new kakao.maps.Map(container.current, options);
+	}, []);
+
 	return (
 		<Layout name={'Location'}>
-			<p>Location</p>
+			<div id='map' ref={container}></div>
 		</Layout>
 	);
 }
