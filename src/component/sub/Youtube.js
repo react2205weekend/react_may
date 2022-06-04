994c3474a6ab467363975c2aaa9a847556abc488
tubi55 <tubi55@nate.com>
2022-06-04 09:45:53 +0900

    데이터 fetching함수 분리

--- src/component/sub/Youtube.js
+++ src/component/sub/Youtube.js
@@ -8,22 +8,24 @@ function Youtube() {
 	const [Open, setOpen] = useState(false);
 	const [Index, setIndex] = useState(0);
 
-	const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
-	const playlist = 'PLHtvRFLN5v-W-izd7V4JH2L4-RTW0WRi3';
-	const num = 8;
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+	const fetchYoutube = () => {
+		const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
+		const playlist = 'PLHtvRFLN5v-W-izd7V4JH2L4-RTW0WRi3';
+		const num = 8;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+
+		axios.get(url).then((json) => {
+			console.log(json.data.items);
+			setVids(json.data.items);
+		});
+	};
 
 	const handlePopup = (index) => {
 		setOpen(true);
 		setIndex(index);
 	};
 
-	useEffect(() => {
-		axios.get(url).then((json) => {
-			console.log(json.data.items);
-			setVids(json.data.items);
-		});
-	}, []);
+	useEffect(fetchYoutube, []);
 
 	return (
 		<>
