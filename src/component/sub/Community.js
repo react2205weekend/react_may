6baf5c42b5f6f3c0951f506091f78697e416f936
tubi55 <tubi55@nate.com>
2022-06-11 11:24:16 +0900

    기본 글 입력 기능 추가

--- src/component/sub/Community.js
+++ src/component/sub/Community.js
@@ -1,9 +1,62 @@
 import Layout from '../common/Layout';
+import { useRef, useState, useEffect } from 'react';
 
 function Community() {
+	const input = useRef(null);
+	const textarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+
+	const resetPost = () => {
+		input.current.value = '';
+		textarea.current.value = '';
+	};
+
+	const createPost = () => {
+		if (
+			input.current.value.trim() === '' ||
+			textarea.current.value.trim() === ''
+		) {
+			resetPost();
+			return alert('제목과 본문을 입력하세요');
+		}
+		setPosts([
+			{ title: input.current.value, content: textarea.current.value },
+			...Posts,
+		]);
+
+		resetPost();
+	};
+
+	useEffect(() => {
+		console.log(Posts);
+	}, [Posts]);
+
 	return (
 		<Layout name={'Community'}>
-			<p>Community</p>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요' ref={input} />
+				<br />
+				<textarea
+					cols='30'
+					rows='5'
+					placeholder='본문을 입력하세요'
+					ref={textarea}></textarea>
+				<br />
+
+				<button onClick={resetPost}>CANCEL</button>
+				<button onClick={createPost}>WRITE</button>
+			</div>
+
+			<div className='showBox'>
+				{Posts.map((post, idx) => {
+					return (
+						<article key={idx}>
+							<h2>{post.title}</h2>
+							<p>{post.content}</p>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
