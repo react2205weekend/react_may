37e6a74e072fee5bff19abc6c63079a0064a4c63
tubi55 <tubi55@nate.com>
2022-06-12 10:46:17 +0900

    메인페이지에 미리보기 데이터 출력

--- src/component/main/News.js
+++ src/component/main/News.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 function News() {
-	return <section id='news'>Content</section>;
+	const getLocalData = () => {
+		const data = localStorage.getItem('post');
+		return JSON.parse(data);
+	};
+
+	const [Posts] = useState(getLocalData());
+
+	useEffect(() => {}, []);
+
+	return (
+		<section id='news'>
+			{Posts.map((post, idx) => {
+				if (idx < 4) {
+					return (
+						<article key={idx}>
+							<h2>{post.title}</h2>
+							<p>{post.content}</p>
+						</article>
+					);
+				}
+			})}
+		</section>
+	);
 }
 
 export default News;
