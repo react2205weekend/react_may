3bfa500a0c40d442300b1eae4c34f2f401b7ac2c
tubi55 <tubi55@nate.com>
2022-06-18 11:35:40 +0900

    슬라이더 ui styling

--- src/component/main/Visual.js
+++ src/component/main/Visual.js
@@ -2,7 +2,40 @@ import Anime from '../../asset/anim.js';
 import { useRef } from 'react';
 
 function Visual() {
-	return <figure id='visual' className='myScroll'></figure>;
+	return (
+		<figure id='visual' className='myScroll'>
+			<article id='slider'>
+				<ul className='panel'>
+					<li className='s1 on'>
+						<span>1</span>
+					</li>
+					<li className='s2'>
+						<span>2</span>
+					</li>
+					<li className='s3'>
+						<span>3</span>
+					</li>
+					<li className='s4'>
+						<span>4</span>
+					</li>
+					<li className='s5'>
+						<span>5</span>
+					</li>
+				</ul>
+
+				<ul className='navi'>
+					<li className='on'></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+
+				<button className='prev'></button>
+				<button className='next'></button>
+			</article>
+		</figure>
+	);
 }
 
 export default Visual;
