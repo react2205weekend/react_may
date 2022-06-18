3ec64d174900277c698d371f9bb1edcb0a2be695
tubi55 <tubi55@nate.com>
2022-06-18 10:50:08 +0900

    원하는 위치에서 스타일 적용시작

--- src/component/main/Pics.js
+++ src/component/main/Pics.js
@@ -1,10 +1,9 @@
-import { startTransition } from 'react';
-
 function Pics({ Scrolled, start, base }) {
 	const position = Scrolled - start - base;
 
 	return (
 		<section id='pics' className='myScroll'>
+			{/* <p style={position >= 0 ? { left: 100 + position } : null}>FLICKR</p> */}
 			<p style={{ left: 100 + position }}>FLICKR</p>
 		</section>
 	);
