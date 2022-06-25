860f36cb2c34201b75c8735ae2b517b23cd3bd97
tubi55 <tubi55@nate.com>
2022-06-25 09:39:30 +0900

    flickr데이터 가져오기

--- src/component/sub/Gallery.js
+++ src/component/sub/Gallery.js
@@ -1,6 +1,22 @@
+import axios from 'axios';
 import Layout from '../common/Layout';
+import { useEffect, useState } from 'react';
 
 function Gallery() {
+	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+	const method_interest = 'flickr.interestingness.getList';
+	const num = 50;
+	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${num}&format=json&nojsoncallback=1`;
+
+	const [Items, setItems] = useState([]);
+
+	useEffect(() => {
+		axios.get(url).then((json) => {
+			console.log(json.data.photos.photo);
+			setItems(json.data.photos.photo);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Gallery'}>
 			<p>Gallery</p>
