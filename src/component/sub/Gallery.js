e645e608720d957a64241f2a1e7a76a74511446c
tubi55 <tubi55@nate.com>
2022-06-25 11:41:36 +0900

    검색어 입력후 검색 버튼 클릭시 이미지 출력

--- src/component/sub/Gallery.js
+++ src/component/sub/Gallery.js
@@ -1,19 +1,26 @@
 import axios from 'axios';
 import Layout from '../common/Layout';
-import { useEffect, useState, useRef } from 'react';
+import {
+	useEffect,
+	useState,
+	useRef,
+} from 'react';
 import Masonry from 'react-masonry-component';
 
 function Gallery() {
 	const frame = useRef(null);
+	const input = useRef(null);
 	const [Items, setItems] = useState([]);
 	const [Loading, setLoading] = useState(true);
-	const [EnableClick, setEnableClick] = useState(false);
+	const [EnableClick, setEnableClick] =
+		useState(false);
 	const masonryOption = {
 		transitionDuration: '0.5s',
 	};
 
 	const getFlickr = async (opt) => {
-		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+		const key =
+			'4612601b324a2fe5a1f5f7402bf8d87a';
 		const method_interest =
 			'flickr.interestingness.getList';
 		const method_search = 'flickr.photos.search';
@@ -36,7 +43,11 @@ function Gallery() {
 	};
 
 	useEffect(
-		() => getFlickr({ type: 'interest', count: 50 }),
+		() =>
+			getFlickr({
+				type: 'interest',
+				count: 50,
+			}),
 		[]
 	);
 
@@ -47,20 +58,48 @@ function Gallery() {
 					if (!EnableClick) return;
 					setLoading(true);
 					frame.current.classList.remove('on');
-					getFlickr({ type: 'interest', count: 50 });
+					getFlickr({
+						type: 'interest',
+						count: 50,
+					});
 					setEnableClick(false);
 				}}>
 				Interest Gallery
 			</button>
 
+			<div className='searchBox'>
+				<input type='text' ref={input} />
+				<button
+					onClick={() => {
+						if (!EnableClick) return;
+						setEnableClick(false);
+						setLoading(true);
+						frame.current.classList.remove('on');
+
+						const result = input.current.value;
+						getFlickr({
+							type: 'search',
+							count: 50,
+							tags: result,
+						});
+					}}>
+					search
+				</button>
+			</div>
+
 			{Loading && (
 				<img
 					className='loading'
-					src={process.env.PUBLIC_URL + '/img/loading.gif'}
+					src={
+						process.env.PUBLIC_URL +
+						'/img/loading.gif'
+					}
 				/>
 			)}
 			<article ref={frame}>
-				<Masonry elementType={'ul'} options={masonryOption}>
+				<Masonry
+					elementType={'ul'}
+					options={masonryOption}>
 					{Items.map((item) => {
 						return (
 							<li key={item.id}>
