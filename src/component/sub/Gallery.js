9c008c13b58f31542b6abb47f3320945874626bd
tubi55 <tubi55@nate.com>
2022-06-25 10:44:33 +0900

    masonry 컴포넌트 적용

--- src/component/sub/Gallery.js
+++ src/component/sub/Gallery.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import Layout from '../common/Layout';
 import { useEffect, useState, useRef } from 'react';
+import Masonry from 'react-masonry-component';
 
 function Gallery() {
 	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
@@ -11,6 +12,9 @@ function Gallery() {
 	const url_search = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${key}&per_page=${num}&tags=ocean&format=json&nojsoncallback=1`;
 	const frame = useRef(null);
 	const [Items, setItems] = useState([]);
+	const masonryOption = {
+		transitionDuration: '0.5s',
+	};
 
 	const getFlickr = async (url) => {
 		await axios.get(url).then((json) => {
@@ -39,23 +43,25 @@ function Gallery() {
 				}}>
 				Search Gallery
 			</button>
-			<ul ref={frame}>
-				{Items.map((item) => {
-					return (
-						<li key={item.id}>
-							<div className='inner'>
-								<div className='pic'>
-									<img
-										src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-										alt={item.title}
-									/>
+			<div ref={frame}>
+				<Masonry elementType={'ul'} options={masonryOption}>
+					{Items.map((item) => {
+						return (
+							<li key={item.id}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+											alt={item.title}
+										/>
+									</div>
+									<h2>{item.title}</h2>
 								</div>
-								<h2>{item.title}</h2>
-							</div>
-						</li>
-					);
-				})}
-			</ul>
+							</li>
+						);
+					})}
+				</Masonry>
+			</div>
 		</Layout>
 	);
 }
