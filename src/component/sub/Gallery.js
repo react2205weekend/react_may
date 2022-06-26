19f18bcfbd0da1d77187292ede80679232fff625
tubi55 <tubi55@nate.com>
2022-06-26 09:29:49 +0900

    사용자 프로필 정보 출력

--- src/component/sub/Gallery.js
+++ src/component/sub/Gallery.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 import Layout from '../common/Layout';
-import {
-	useEffect,
-	useState,
-	useRef,
-} from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Masonry from 'react-masonry-component';
 
 function Gallery() {
@@ -12,26 +8,28 @@ function Gallery() {
 	const input = useRef(null);
 	const [Items, setItems] = useState([]);
 	const [Loading, setLoading] = useState(true);
-	const [EnableClick, setEnableClick] =
-		useState(false);
+	const [EnableClick, setEnableClick] = useState(false);
 	const masonryOption = {
 		transitionDuration: '0.5s',
 	};
 
 	const getFlickr = async (opt) => {
-		const key =
-			'4612601b324a2fe5a1f5f7402bf8d87a';
-		const method_interest =
-			'flickr.interestingness.getList';
+		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+		const method_interest = 'flickr.interestingness.getList';
 		const method_search = 'flickr.photos.search';
+		const method_user = 'flickr.people.getPhotos';
 		let url = '';
 		if (opt.type === 'interest')
 			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${key}&per_page=${opt.count}&format=json&nojsoncallback=1`;
 		if (opt.type === 'search')
 			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${key}&per_page=${opt.count}&tags=${opt.tags}&format=json&nojsoncallback=1`;
+		if (opt.type === 'user')
+			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${key}&per_page=${opt.count}&user_id=${opt.user}&format=json&nojsoncallback=1`;
 
 		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
+			if (json.data.photos.photo.length === 0)
+				return alert('해당 검색어의 결과값이 없습니다.');
 			setItems(json.data.photos.photo);
 		});
 
@@ -53,7 +51,7 @@ function Gallery() {
 		setEnableClick(false);
 	};
 
-	const showSearch = (e) => {
+	const showSearch = () => {
 		const result = input.current.value.trim();
 		if (!result) {
 			return alert('검색어를 입력하세요');
@@ -83,30 +81,28 @@ function Gallery() {
 
 	return (
 		<Layout name={'Gallery'}>
-			<button onClick={showInterest}>
-				Interest Gallery
-			</button>
+			<button onClick={showInterest}>Interest Gallery</button>
 
 			<div className='searchBox'>
-				<input type='text' ref={input} />
-				<button onClick={showSearch}>
-					search
-				</button>
+				<input
+					type='text'
+					ref={input}
+					onKeyUp={(e) => {
+						if (e.key === 'Enter') showSearch();
+					}}
+				/>
+				<button onClick={showSearch}>search</button>
 			</div>
 
 			{Loading && (
 				<img
 					className='loading'
-					src={
-						process.env.PUBLIC_URL +
-						'/img/loading.gif'
-					}
+					src={process.env.PUBLIC_URL + '/img/loading.gif'}
 				/>
 			)}
+
 			<article ref={frame}>
-				<Masonry
-					elementType={'ul'}
-					options={masonryOption}>
+				<Masonry elementType={'ul'} options={masonryOption}>
 					{Items.map((item) => {
 						return (
 							<li key={item.id}>
@@ -118,6 +114,19 @@ function Gallery() {
 										/>
 									</div>
 									<h2>{item.title}</h2>
+									<div className='profile'>
+										<img
+											src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+											alt={item.owner}
+											onError={(e) =>
+												e.target.setAttribute(
+													'src',
+													'https://www.flickr.com/images/buddyicon.gif'
+												)
+											}
+										/>
+										<span>{item.owner}</span>
+									</div>
 								</div>
 							</li>
 						);
