69934b9ccffce3a86ddb546104921759ce621c9f
tubi55 <tubi55@nate.com>
2022-06-26 11:51:47 +0900

    특정 영역안에 마우스 포인터 모양 변경

--- src/component/main/Vids.js
+++ src/component/main/Vids.js
@@ -1,10 +1,22 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from 'swiper';
+import { useRef, useEffect } from 'react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 function Vids() {
+	const cursor = useRef(null);
+	const mouseMove = (e) => {
+		cursor.current.style.left = e.clientX + 'px';
+		cursor.current.style.top = e.clientY + 'px';
+	};
+
+	useEffect(() => {
+		window.addEventListener('mousemove', mouseMove);
+		return () => window.removeEventListener('mousemove', mouseMove);
+	}, []);
+
 	return (
 		<section id='vids' className='myScroll'>
 			<Swiper
@@ -25,7 +37,25 @@ function Vids() {
 						spaceBetween: 50,
 					},
 				}}>
-				<SwiperSlide>
+				{[0, 1, 2, 3, 4, 5].map((num) => {
+					return (
+						<SwiperSlide key={num}>
+							<div
+								className='inner'
+								onMouseEnter={() =>
+									(cursor.current.style =
+										'transform: translate(-50%, -50%) scale(3) ')
+								}
+								onMouseLeave={() =>
+									(cursor.current.style =
+										'transform: translate(-50%, -50%) scale(1)  ')
+								}>
+								{num + 1}
+							</div>
+						</SwiperSlide>
+					);
+				})}
+				{/* <SwiperSlide>
 					<div className='inner'>1</div>
 				</SwiperSlide>
 				<SwiperSlide>
@@ -42,8 +72,9 @@ function Vids() {
 				</SwiperSlide>
 				<SwiperSlide>
 					<div className='inner'>6</div>
-				</SwiperSlide>
+				</SwiperSlide> */}
 			</Swiper>
+			<div className='cursor' ref={cursor}></div>
 		</section>
 	);
 }
