066c6d06238bc89434e9fe3cbaa330bde2429c12
tubi55 <tubi55@nate.com>
2022-07-16 09:43:45 +0900

    미리보기 이미지 줄바꿈 처리

--- src/component/main/Pics.js
+++ src/component/main/Pics.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 function Pics({ Scrolled, start, base }) {
 	const { flickr } = useSelector((store) => store.flickrReducer);
 	let position = 0;
+	const preview_num = 10;
 	if (start) position = Scrolled - start - base;
 
 	return (
@@ -12,7 +13,7 @@ function Pics({ Scrolled, start, base }) {
 			<h3 style={{ left: 100 + position * 2 }}>FLICKR</h3>
 
 			{flickr.map((item, idx) => {
-				if (idx < 5) {
+				if (idx < preview_num) {
 					return (
 						<div className='pic' key={item.id}>
 							<img
